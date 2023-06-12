c8724ed73b08968c8da0bfb07ffae9556da8edc4
DumitrescuMircea <dumitrescumircea17@stud.ase.ro>
2023-06-12 20:37:28 +0300

    name_form & preliminary_form beginning

--- actions/actions.py
+++ actions/actions.py
@@ -5,7 +5,7 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 from datetime import datetime, date
 from rasa_sdk import Action, Tracker
-from rasa_sdk.events import SlotSet
+from rasa_sdk.events import SlotSet, ActiveLoop
 import re
 from datetime import datetime, time
 
@@ -169,4 +169,178 @@ class ValidationBookAppointmentForm(FormValidationAction):
             return {"department": None}
 
         return {"department": slot_value}
+        
+class ValidationNameForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_name_form"
+    def validate_first_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `first_name` value."""
+        # We check for super short first_names
+        name = clean_text(slot_value)
+        if len(name) == 0:
+            dispatcher.utter_message(text="Cred că ați greșit.")
+            return {"first_name": None}
+        return {"first_name": name}
+    def validate_last_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `last_name` value."""
+        name = clean_text(slot_value)
+        if len(name) == 0:
+            dispatcher.utter_message(text="Cred că ați greșit.")
+            return{"last_name": None}
+        first_name = tracker.get_slot("first_name")
+        if len(first_name) + len(name) < 3:
+            dispatcher.utter_message(
+                text="Combinația nume/prenume este prea scurtă. Credem că s-a comis o eroare. Restartăm.")
+            return {"first_name": None, "last_name": None}
+        return {"last_name": name}
+   
+class ValidationPreliminaryQuestionForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_preliminary_questions_form"
+    def validate_gender(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `gender` value."""
+        gender = clean_text(slot_value)
+        if len(gender) == 0:
+            dispatcher.utter_message(text="Ați uitat sa completați sexul")
+            return{"gender": None}
+        return{"gender":gender}
+    def validate_age(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `age` value."""
+        age = clean_numbers(slot_value)
+        if (int(age) <= 0 or int(age) > 100):
+            dispatcher.utter_message(text = "Vă rog să introduceți o vârstă între 1-100 ani")
+            return{"age":None}
+        if (int(age) is None):
+            dispatcher.utter_message(text = "Vă rog să introduceți o vârstă în cifre")
+            return{"age":None}
+        return{"age":age}
+        
+    def validate_weight_risk(self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `weight_risk` value"""
+        intent = tracker.latest_message["intent"].get("name")
+        weight_risk = clean_text(slot_value)
+        if len(weight_risk) == 0:
+            dispatcher.utter_message(text = "Vă rog să răspundeți cu DA sau NU")
+            return{"weight_risk":None}
+        if intent == "weight_risk ":
+            substring = "da"
+            if substring in weight_risk:
+                return{"weight_risk": True}
+            else:
+                return{"weight_risk": False} 
+        return{"weight_risk":weight_risk}
+    
+    def validate_hypertension(self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `hypertension` value"""
+        intent = tracker.latest_message["intent"].get("name")
+        hypertension = clean_text(slot_value)
+        if len(hypertension) == 0:
+            dispatcher.utter_message(text = "Vă rog să răspundeți cu DA sau NU")
+            return{"hypertension":None}
+        if intent == "hypertension":
+            substring = "da"
+            if substring in hypertension:
+                return{"hypertension": True}
+            else:
+                return{"hypertension": False}
+        return{"hypertension":hypertension}
+    
+    def validate_smoker(self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `smoker` value"""
+        intent = tracker.latest_message["intent"].get("name")
+        smoker = clean_text(slot_value)
+        if len(smoker) == 0:
+            dispatcher.utter_message(text = "Vă rog să răspundeți cu DA sau NU")
+            return{"smoker":None}
+        if intent == "smoker":
+            substring = "da"
+            if substring in smoker:
+                return{"smoker": True}
+            else:
+                return{"smoker": False}
+        return{"smoker":smoker}    
+        
+    def validate_recent_surgeries(self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `recent_surgeries` value"""
+        intent = tracker.latest_message["intent"].get("name")
+        recent_surgeries = clean_text(slot_value)
+        if len(recent_surgeries) == 0:
+            dispatcher.utter_message(text = "Vă rog să răspundeți cu DA sau NU")
+            return{"recent_surgeries":None}
+        elif intent == "recent_surgeries":
+            substring = "da"
+            if substring in recent_surgeries:
+                return{"recent_surgeries": True}
+            else:
+                return{"recent_surgeries": False}
+        return{"recent_surgeries":recent_surgeries}  
+    
+class CheckAppointmentFormFilled(Action):
+    def name(self)->Text:
+        return "action_check_appointment_form_filled"
+    
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        appointment_slots = ["date", "time", "doctor", "department"]
+        if all(tracker.get_slot(slot) for slot in appointment_slots):
+            dispatcher.utter_message("utter_ask_additional_info1")
+            return [SlotSet("slot_ask_for_second_form", True)]
+        else:
+            # First form is not filled yet
+            return [SlotSet("slot_ask_for_second_form", False)]
+
+class CheckExtraDetailsFormFilled(Action):
+    def name(self)->Text:
+        return "action_check_extra_details_form_filled"
+    
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        if (tracker.get_slot("slot_ask_for_second_form")== True):
+            dispatcher.utter_message("utter_ask_additional_info2")
+            return [SlotSet("slot_ask_for_third_form", True)]
+        else:
+            # Appointment form is not filled yet
+            return [SlotSet("slot_ask_for_third_form", False)]
         
